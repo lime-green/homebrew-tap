71754ae4efad83b4036e43da512ac96c1c173062
Josh Doncaster-Marsiglio <jmarsiglio@gmail.com>
2015-06-05 00:36:39 -0400

    Update version

--- bcd.rb
+++ bcd.rb
@@ -1,8 +1,8 @@
 class Bcd < Formula
-  homepage ""
-  url "https://github.com/lime-green/bcd/archive/0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "4a822447dd10ce3905e8b3bd17436851827e1bb46f8cb405f977d43f1a188463"
+  homepage "https://github.com/lime-green/bcd"
+  url "https://github.com/lime-green/bcd/archive/0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "2c101d8dbb1df8037aa786b6ad9fe165f791dbb3c207808294d7242b8cf60219"
 
   def install
     bin.install "back_directory.pl"
